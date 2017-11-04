7632b218926ffa1a4bed3ed86ff68c1fbfefb650
Antti Jumpponen <kuumies@gmail.com>
2017-11-04 20:44:06 +0200

    Added externals, created main build file, added test main

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <glm/vec3.hpp>
+#include <iostream>
+
+int main()
+{
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::cout << extensionCount << " extensions supported" << std::endl;
+
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
+    return EXIT_SUCCESS;
+}
