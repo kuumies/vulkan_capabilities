2c8e5b92a0ff5b79b6a2dfc109a8302a1cb5cbd1
ajumpponen <antti.jumpponen@supponor.com>
2017-11-09 19:23:55 +0200

    test code

---
+++ src/vk_instance_2.h
@@ -0,0 +1,100 @@
+/* ---------------------------------------------------------------- *
+   Antti Jumpponen <kuumies@gmail.com>
+   The definition of kuu::vk::Instance class
+ * ---------------------------------------------------------------- */
+ 
+#pragma once
+
+/* ---------------------------------------------------------------- */
+
+#include <memory>
+#include <vector>
+#include <vulkan/vulkan.h>
+
+/* ---------------------------------------------------------------- */
+
+#include "vk_physical_device.h"
+
+namespace kuu
+{
+namespace vk
+{
+
+/* ---------------------------------------------------------------- *
+   A Vulkan instance.
+   
+   Instance allows user to find out the devices with Vulkan-
+   cabability, selecting the device to be used with rendering and
+   creating a logical device for accessing the physical device.
+      
+   To create the instance user needs to set some (optional) infor-
+   mation about the application:
+   
+    * application name
+    * engine name
+    * vulkan extensions to use (e.g. surface extension)
+    * vulkan layers to use (e.g. debug layer)
+    
+   User can use static isExtensionSupported and isLayerSupported 
+   before creating the instance to find out if the required extension 
+   or layer is supported.
+    
+   Instance is created by calling create() function. If the 
+   application information above changes then instance needs to 
+   be recreated. Also all the objects that use the instance needs 
+   to be recreated.
+       
+   Todo:
+   
+    * surface creation has a GLFW dependency that should be removed
+   
+ * ---------------------------------------------------------------- */
+class Instance
+{
+public:
+    // Sets the application name.
+    Instance& setApplicationName(const std::string& applicationName);
+    // Sets the engine name.
+    Instance& setEngineName(const std::string& engineName);
+    // Sets the extensions.
+    Instance& setExtensions(std::vector<std::string>& extensions);
+    // Sets the layers. If the layers contains the validation layer
+    // then a debug callback is created. The callback will print
+    // issues reported by the validation layer into standard error
+    // output.
+    Instance& setLayers(std::vector<std::string>& layers);
+    
+    // Creates the instance.
+    bool create();
+    // Destroys the instance.
+    bool destroy();
+    
+    // Returns handle to Vulkan instance. If the instance has not been 
+    // created or it has been destroyd then a std::runtime_exection is
+    // thrown.
+    vkInstance handle() const;
+    
+    // Returns all physical devices. If the instance is has not been 
+    // created or it has been deleted then a std::runtime_exception is 
+    // thrown.
+    std::vector<PhysicalDevice> physicalDevices() const;
+
+    // Returns an instance of the class.
+    static Instance& get();
+    // Returns true if the instance supports the given in extension.
+    static bool isExtensionSupported(const std::string& extension);
+    // Returns true if the instance supports the given in layer..
+    static bool isLayerSupported(const std::string& layer);
+    
+private:
+    Instance();
+    Instance(Instance const&);
+    void operator=(Instance const&);
+    
+private:
+    struct Data;
+    std::shared_ptr<Data> d;
+};
+
+} // namespace vk
+} // namespace kuu
