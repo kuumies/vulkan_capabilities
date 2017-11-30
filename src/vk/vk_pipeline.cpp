e54ea65d70768df27c921d433d41749ed770d68f
Antti Jumpponen <kuumies@gmail.com>
2017-11-30 08:02:27 +0200

    worked with pipeline, does not compile

--- src/vk/vk_pipeline.cpp
+++ src/vk/vk_pipeline.cpp
@@ -31,7 +31,8 @@ struct Pipeline::Impl
         info.pStages = shaderStages.data();
         info.pVertexInputState = &vertexInputState;
         info.pInputAssemblyState = &inputAssemblyState;
-
+        info.pViewportState = &viewportState;
+        info.pRasterizationState = &rasterizerState;
 
 
         VkResult result;
@@ -62,6 +63,12 @@ struct Pipeline::Impl
     std::vector<VkPipelineShaderStageCreateInfo> shaderStages;
     VkPipelineVertexInputStateCreateInfo vertexInputState;
     VkPipelineInputAssemblyStateCreateInfo inputAssemblyState;
+    VkPipelineViewportStateCreateInfo viewportState;
+    VkPipelineRasterizationStateCreateInfo rasterizerState;
+    VkPipelineMultisampleStateCreateInfo multisampleState;
+    VkPipelineDepthStencilStateCreateInfo depthStencilState;
+    VkPipelineColorBlendStateCreateInfo colorBlendState;
+    VkPipelineDynamicStateCreateInfo dynamicState;
 };
 
 /* -------------------------------------------------------------------------- */
@@ -96,10 +103,10 @@ Pipeline& Pipeline::setVertexInputState(
     vertexInputState.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
     vertexInputState.pNext = NULL;
     vertexInputState.flags = 0;
-    vertexInputState.vertexBindingDescriptionCount = uint32_t(bindingDescs.size());
-    vertexInputState.pVertexBindingDescriptions = bindingDescs.data();
+    vertexInputState.vertexBindingDescriptionCount   = uint32_t(bindingDescs.size());
+    vertexInputState.pVertexBindingDescriptions      = bindingDescs.data();
     vertexInputState.vertexAttributeDescriptionCount = uint32_t(attributeDescs.size());
-    vertexInputState.pVertexAttributeDescriptions= attributeDescs.data();
+    vertexInputState.pVertexAttributeDescriptions    = attributeDescs.data();
     return setVertexInputState(vertexInputState);
 }
 
@@ -118,10 +125,10 @@ Pipeline& Pipeline::setInputAssemblyState(
     VkBool32 primitiveRestartEnable)
 {
     VkPipelineInputAssemblyStateCreateInfo state;
-    state.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
-    state.pNext = NULL;
-    state.flags = 0;
-    state.topology = topology;
+    state.sType                  = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+    state.pNext                  = NULL;
+    state.flags                  = 0;
+    state.topology               = topology;
     state.primitiveRestartEnable = primitiveRestartEnable;
 
     return setInputAssemblyState(state);
@@ -129,6 +136,189 @@ Pipeline& Pipeline::setInputAssemblyState(
 
 VkPipelineInputAssemblyStateCreateInfo Pipeline::inputAssemblyState() const
 { return impl->inputAssemblyState; }
+
+Pipeline& Pipeline::setViewportState(const VkPipelineViewportStateCreateInfo& state)
+{
+    impl->viewportState = state;
+    return *this;
+}
+
+Pipeline& Pipeline::setViewportState(
+    const std::vector<VkViewport>& viewports,
+    const std::vector<VkRect2D>& scissors)
+{
+    VkPipelineViewportStateCreateInfo state;
+    state.sType         = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+    state.pNext         = NULL;
+    state.flags         = 0;
+    state.viewportCount = uint32_t(viewports.size());
+    state.pViewports    = viewports.data();
+    state.scissorCount  = uint32_t(scissors.size());
+    state.pScissors     = scissors.data();
+    return setViewportState(state);
+}
+
+VkPipelineViewportStateCreateInfo Pipeline::viewportState() const
+{ return impl->viewportState; }
+
+Pipeline& Pipeline::setRasterizerState(const VkPipelineRasterizationStateCreateInfo& state)
+{
+    impl->rasterizeState = state;
+    return *this;
+}
+
+Pipeline& Pipeline::setRasterizerState(
+    VkPolygonMode polygonMode,
+    VkCullModeFlags cullMode,
+    VkFrontFace frontFace,
+    VkBool32 depthClampEnable,
+    VkBool32 rasterizerDiscardEnable,
+    VkBool32 depthBiasEnable,
+    float depthBiasConstantFactor,
+    float depthBiasClamp,
+    float depthBiasSlopeFactor,
+    float lineWidth);
+{
+    VkPipelineRasterizationStateCreateInfo state;
+    state.sType                   = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+    state.pNext                   = NULL;
+    state.flags                   = 0;
+    state.depthClampEnable        = depthClampEnable;
+    state.rasterizerDiscardEnable = rasterizerDiscardEnable;
+    state.polygonMode             = polygonMode;
+    state.cullMode                = cullMode;
+    state.frontFace               = frontFace;
+    state.depthBiasEnable         = depthBiasEnable;
+    state.depthBiasConstantFactor = depthBiasConstantFactor;
+    state.depthBiasClamp          = depthBiasClamp;
+    state.depthBiasSlopeFactor    = depthBiasSlopeFactor;
+    state.lineWidth               = lineWidth;
+
+    return setRasterizerState(state);
+}
+
+VkPipelineRasterizationStateCreateInfo Pipeline::rasterizeState() const
+{ return impl->rasterizeState; }
+
+Pipeline& Pipeline::setMultisampleState(const VkPipelineMultisampleStateCreateInfo& state)
+{
+    impl->multisampleState = state;
+    return *this;
+}
+
+Pipeline& Pipeline::setMultisampleState(
+    VkBool32 sampleShadingEnable,
+    VkSampleCountFlagBits rasterizationSamples,
+    float minSampleShading,
+    const VkSampleMask* pSampleMask,
+    VkBool32 alphaToCoverageEnable,
+    VkBool32 alphaToOneEnable)
+{
+    VkPipelineMultisampleStateCreateInfo state;
+    state.sType                 = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+    state.pNext                 = NULL;
+    state.flags                 = 0;
+    state.rasterizationSamples  = rasterizationSamples;
+    state.sampleShadingEnable   = sampleShadingEnable;
+    state.minSampleShading      = minSampleShading;
+    state.pSampleMask           = pSampleMask;
+    state.alphaToCoverageEnable = alphaToCoverageEnable;
+    state.alphaToOneEnable      = alphaToOneEnable;
+
+    return setMultisampleState(state);
+}
+
+VkPipelineMultisampleStateCreateInfo Pipeline::multisampleState() const
+{ return impl->multisampleState; }
+
+
+Pipeline& Pipeline::setDepthStencilState(const VkPipelineDepthStencilStateCreateInfo& state)
+{
+    impl->depthStencilState = state;
+    return *this;
+}
+
+Pipeline& Pipeline::setDepthStencilState(
+    VkBool32 depthTestEnable,
+    VkBool32 depthWriteEnable,
+    VkCompareOp depthCompareOpm,
+    VkBool32 depthBoundsTestEnable,
+    VkBool32 stencilTestEnable,
+    VkStencilOpState front,
+    VkStencilOpState back,
+    float minDepthBounds,
+    float maxDepthBounds)
+{
+    VkPipelineDepthStencilStateCreateInfo state;
+    state.sType                 = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO;
+    state.pNext                 = NULL;
+    state.flags                 = 0;
+    state.depthTestEnable       = depthTestEnable;
+    state.depthWriteEnable      = depthWriteEnable;
+    state.depthCompareOp        = depthCompareOp;
+    state.depthBoundsTestEnable = depthBoundsTestEnable;;
+    state.stencilTestEnable     = stencilTestEnable;
+    state.front                 = front;
+    state.back                  = back;;
+    state.minDepthBounds        = minDepthBounds;
+    state.maxDepthBounds        = maxDepthBounds;
+
+    return setDepthStencilState(state);
+}
+
+VkPipelineDepthStencilStateCreateInfo depthStencilState() const
+{ return impl->depthStencilState; }
+
+
+// Sets  and gets the color blending state.
+Pipeline& Pipeline::setColorBlendingState(const VkPipelineColorBlendStateCreateInfo& state)
+{
+    impl->colorBlendState = state;
+    return *this;
+}
+
+Pipeline& Pipeline::setColorBlendingState(
+    VkBool32 logicOpEnable,
+    VkLogicOp logicOp,
+    const std::vector<VkPipelineColorBlendAttachmentState> attachments,
+    float blendConstants[4])
+{
+
+}
+
+VkPipelineColorBlendStateCreateInfo Pipeline::colorBlendingState() const
+{ return impl->colorBlendState; }
+
+Pipeline& Pipeline::setDynamicState(const VkPipelineDynamicStateCreateInfo& state)
+{
+    impl->dynamicState = state;
+    return *this;
+}
+
+Pipeline& Pipeline::setDynamicState(const std::vector<VkDynamicState>& states)
+{
+
+}
+
+VkPipelineDynamicStateCreateInfo Pipeline::dynamicState() const
+{ return impl->dynamicState; }
+
+Pipeline& Pipeline::setPipelineLayout(const VkPipelineLayoutCreateInfo& layout)
+{
+
+}
+
+Pipeline& Pipeline::setPipelineLayout(
+    std::vector<VkDescriptorSetLayout>& layouts,
+    std::vector<VkPushConstantRange>& pushConstantRanges)
+{
+
+}
+
+VkPipelineLayoutCreateInfo Pipeline::pipelineLayout() const
+{
+
+}
 
 void Pipeline::create()
 {
--- src/vk/vk_pipeline.h
+++ src/vk/vk_pipeline.h
@@ -44,6 +44,74 @@ public:
         VkBool32 primitiveRestartEnable);
     VkPipelineInputAssemblyStateCreateInfo inputAssemblyState() const;
 
+    // Sets and gets the viewport state.
+    Pipeline& setViewportState(const VkPipelineViewportStateCreateInfo& state);
+    Pipeline& setViewportState(
+        const std::vector<VkViewport>& viewports,
+        const std::vector<VkRect2D>& scissors);
+    VkPipelineViewportStateCreateInfo viewportState() const;
+
+    // Sets and gets the rasterized state.
+    Pipeline& setRasterizerState(const VkPipelineRasterizationStateCreateInfo& state);
+    Pipeline& setRasterizerState(
+        VkPolygonMode polygonMode,
+        VkCullModeFlags cullMode,
+        VkFrontFace frontFace,
+        VkBool32 depthClampEnable = VK_FALSE,
+        VkBool32 rasterizerDiscardEnable = VK_FALSE,
+        VkBool32 depthBiasEnable = VK_FALSE;,
+        float depthBiasConstantFactor = 0.0f,
+        float depthBiasClamp = 0.0f,
+        float depthBiasSlopeFactor = 0.0f,
+        float lineWidth = 1.0f);
+    VkPipelineRasterizationStateCreateInfo rasterizeState() const;
+
+    // Sets and gets the multisample state.
+    Pipeline& setMultisampleState(const VkPipelineMultisampleStateCreateInfo& state);
+    Pipeline& setMultisampleState(
+        VkBool32 sampleShadingEnable,
+        VkSampleCountFlagBits rasterizationSamples,
+        float minSampleShading = 1.0f,
+        const VkSampleMask* pSampleMask = NULL,
+        VkBool32 alphaToCoverageEnable = VK_FALSE,
+        VkBool32 alphaToOneEnable = VK_FALSE);
+    VkPipelineMultisampleStateCreateInfo multisampleState() const;
+
+    // Sets and gets the depth/stencil state.
+    Pipeline& setDepthStencilState(const VkPipelineDepthStencilStateCreateInfo& state);
+    Pipeline& setDepthStencilState(
+        VkBool32 depthTestEnable,
+        VkBool32 depthWriteEnable = VK_TRUE,
+        VkCompareOp depthCompareOpm = VK_COMPARE_OP_LESS_OR_EQUAL,
+        VkBool32 depthBoundsTestEnable = VK_FALSE,
+        VkBool32 stencilTestEnable = VK_FALSE,
+        VkStencilOpState front = {},
+        VkStencilOpState back = {},
+        float minDepthBounds = 0.0f,
+        float maxDepthBounds = 0.0f);
+    VkPipelineDepthStencilStateCreateInfo depthStencilState() const;
+
+    // Sets  and gets the color blending state.
+    Pipeline& setColorBlendingState(const VkPipelineColorBlendStateCreateInfo& state);
+    Pipeline& setColorBlendingState(
+        VkBool32 logicOpEnable,
+        VkLogicOp logicOp,
+        const std::vector<VkPipelineColorBlendAttachmentState> attachments,
+        float blendConstants[4]);
+    VkPipelineColorBlendStateCreateInfo colorBlendingState() const;
+
+    // Sets and gets the dynamic state.
+    Pipeline& setDynamicState(const VkPipelineDynamicStateCreateInfo& state);
+    Pipeline& setDynamicState(const std::vector<VkDynamicState>& states);
+    VkPipelineDynamicStateCreateInfo dynamicState() const;
+
+    // Sets and gets the pipeline layout.
+    Pipeline& setPipelineLayout(const VkPipelineLayoutCreateInfo& layout);
+    Pipeline& setPipelineLayout(std::vector<VkDescriptorSetLayout>& layouts,
+                                std::vector<VkPushConstantRange>& pushConstantRanges);
+    VkPipelineLayoutCreateInfo pipelineLayout() const;
+
+
     // Creates and destroys the pipeline.
     void create();
     void destroy();
@@ -53,6 +121,7 @@ public:
 
     // Returns the handle.
     VkPipeline handle() const;
+    VkPipelineLayout pipelineLayoutHandle() const;
 
 private:
     struct Impl;
