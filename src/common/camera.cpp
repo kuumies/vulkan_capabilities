259bc8cee4d10203a3cd391472f050b7ed8f0f06
ajumpponen <antti.jumpponen@supponor.com>
2017-12-19 17:22:56 +0200

    Move def from header to cpp

--- src/common/camera.cpp
+++ src/common/camera.cpp
@@ -4,6 +4,9 @@
  * -------------------------------------------------------------------------- */
 
 #include "camera.h"
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/geometric.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
